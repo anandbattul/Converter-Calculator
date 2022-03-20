2a4408117ce01d73292fd5fe87ce38ba8376a362
ANAND BATTUL <92909928+anandbattul@users.noreply.github.com>
2022-03-20 13:27:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function cal() {
+    let val = document.getElementById("secd").value;
+    if (val === "" || isNaN(val)) {
+        alert("Please enter the time in Seconds")
+    } else {
+        let years = 0
+        let days = 0;
+        let sec = val % 60
+        let min = val / 60
+        let hr = min / 60
+        if (hr > 24) {
+            days = hr / 24
+            hr = hr % 24
+        }
+        if (min > 60) {
+            min = min % 60
+        };
+        if (days > 365) {
+            years = days / 365
+            days = days % 365
+        }
+        document.getElementById("dis").innerHTML = ("Years:" + parseInt(years) + "&nbsp " + "Days:" + parseInt(days) + "&nbsp " + "Hours:" + parseInt(hr) + "&nbsp " + 
+"Minunts:" + parseInt(min) + "&nbsp " + "Seconds:" + (sec))
+        document.getElementById("dis2").innerHTML = "Thank You"
+    }
+}
